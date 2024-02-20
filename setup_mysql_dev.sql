53b05f3909620d7fde0a7f3844d720050aae4ef1
bkaoukine <b.koukin@gmail.com>
2024-02-20 21:24:48 +0100

    3. MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,11 @@
--- A script that prepares a MySQL server for the HBNB Cloning Project:
--- A database hbnb_dev_db
+-- Create the database if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
+-- Create the user if it doesn't already exist, and set the password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-CREATE DATABASE IF NOT EXISTS bnb_dev_db;
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-
-USE bnb_dev_db;
-
-GRANT ALL ON bnb_dev_db TO hbnb_dev;
-
-GRANT SELECT ON performance_schema TO hbnb_dev;
+-- Grant SELECT privilege on performance_schema database to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
