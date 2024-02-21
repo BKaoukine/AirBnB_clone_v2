cc64d24608e00dc369cd4c51ed0c6f2cf539293a
bkaoukine <b.koukin@gmail.com>
2024-02-21 23:05:01 +0100

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
-
-class User(BaseModel):
+Base = declarative_base()
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    __tablename__ = "users"
+
+    email = Column("email", String(128), nullable=False)
+    password = Column("password", String(128), nullable=False)
+    first_name = Column("first_name", String(128), nullable=False)
+    last_name = Column("last_name", String(128), nullable=False)
+
